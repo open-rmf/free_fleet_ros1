2f427123b9ecf57835951297d2205a0da620e519
Aaron Chong <aaronchongth@gmail.com>
2020-09-24 19:31:30 +0800

    Stop and resume, need RAII class for TF listener
    Signed-off-by: Aaron Chong <aaronchongth@gmail.com>

--- src/agv/NavStackCommandHandle.cpp
+++ src/agv/NavStackCommandHandle.cpp
@@ -17,7 +17,12 @@
 
 #include <mutex>
 #include <thread>
+#include <deque>
 
+#include <tf2/LinearMath/Matrix3x3.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+
+#include <geometry_msgs/Quaternion.h>
 #include <move_base_msgs/MoveBaseGoal.h>
 #include <move_base_msgs/MoveBaseAction.h>
 #include <actionlib/client/simple_action_client.h>
@@ -27,9 +32,17 @@
 namespace free_fleet {
 namespace agv {
 
+//==============================================================================
 class NavStackCommandHandle::Implementation
 {
 public:
+
+  struct Goal
+  {
+    rmf_traffic::agv::Plan::Waypoint waypoint;
+    move_base_msgs::MoveBaseGoal goal;
+    bool sent = false;
+  };
 
   using MoveBaseClient =
     actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction>;
@@ -40,51 +53,72 @@ public:
 
   Implementation(const Implementation& other)
   {
-    std::unique_lock<std::mutex> other_lock(other._mutex);
-
-    _move_base_client = other._move_base_client;
-    _destination = other._destination;
-    _destination_time = other._destination_time;
-    _graph_index = other._graph_index;
-    _destination_reached_callback = other._destination_reached_callback;
-    _docking_finished_callback = other._docking_finished_callback;
+    // Empty copy constructor, only needed during construction of impl_ptr
+    // All members will be initialized or assigned during runtime.
   }
 
-  Implementation& operator=(const Implementation& other)
+  ~Implementation()
   {
-    if (this != &other)
-    {
-      std::unique_lock<std::mutex> my_lock(_mutex, std::defer_lock);
-      std::unique_lock<std::mutex> other_lock(other._mutex, std::defer_lock);
-      std::lock(my_lock, other_lock);
-
-      _move_base_client = other._move_base_client;
-      _destination = other._destination;
-      _destination_time = other._destination_time;
-      _graph_index = other._graph_index;
-      _destination_reached_callback = other._destination_reached_callback;
-      _docking_finished_callback = other._docking_finished_callback;
-    }
-    return *this;
+    if (_thread.joinable())
+      _thread.join();
   }
 
-  ~Implementation() = default;
+  void _navigation_thread_fn()
+  {
+    while (_node->ok())
+    {
+      // check goal state
+      // pop and send if needed
+      _navigation_handle_rate->sleep();
+    }
+  }
+
+  void _start()
+  {
+    ROS_INFO("Starting navigation thread.");
+    _thread =
+      std::thread(std::bind(&Implementation::_navigation_thread_fn, this));
+  }
+
+  geometry_msgs::Quaternion _get_quat_from_yaw(double yaw) const
+  {
+    tf2::Quaternion quat_tf;
+    quat_tf.setRPY(0.0, 0.0, yaw);
+    quat_tf.normalize();
+    geometry_msgs::Quaternion quat = tf2::toMsg(quat_tf);
+    return quat;
+  }
+
+  move_base_msgs::MoveBaseGoal _waypoint_to_move_base_goal(
+      const rmf_traffic::agv::Plan::Waypoint& waypoint) const
+  {
+    const Eigen::Vector3d& pos = waypoint.position();
+    move_base_msgs::MoveBaseGoal goal;
+    goal.target_pose.header.frame_id = _map_frame;
+    goal.target_pose.header.stamp = ros::Time::now();
+    goal.target_pose.pose.position.x = pos[0];
+    goal.target_pose.pose.position.y = pos[1];
+    goal.target_pose.pose.position.z = 0.0; // TODO: handle Z height with level
+    goal.target_pose.pose.orientation = _get_quat_from_yaw(pos[2]);
+    return goal;
+  }
 
   std::thread _thread;
   mutable std::mutex _mutex;
 
+  std::shared_ptr<ros::NodeHandle> _node;
   std::shared_ptr<MoveBaseClient> _move_base_client;
+  std::unique_ptr<ros::Rate> _navigation_handle_rate;
 
-  Eigen::Vector3d _destination;
-  rmf_traffic::Time _destination_time;
-  rmf_utils::optional<std::size_t> _graph_index;
+  std::string _map_frame;
 
-  RequestCompleted _destination_reached_callback;
+  std::deque<Goal> _goal_path;
+  RequestCompleted _path_finished_callback;
+
   RequestCompleted _docking_finished_callback;
 };
 
 //==============================================================================
-
 NavStackCommandHandle::SharedPtr NavStackCommandHandle::make(
   std::shared_ptr<ros::NodeHandle> node,
   const std::string& move_base_server_name,
@@ -108,46 +142,85 @@ NavStackCommandHandle::SharedPtr NavStackCommandHandle::make(
   ROS_INFO("Connected with move base action server: %s",
       move_base_server_name.c_str());
 
+  command_handle->_pimpl->_node = std::move(node);
   command_handle->_pimpl->_move_base_client = std::move(move_base_client);
+  command_handle->_pimpl->_navigation_handle_rate.reset(
+    new ros::Rate(1));
+  command_handle->_pimpl->_start();
   return command_handle;
 }
 
 //==============================================================================
-
 NavStackCommandHandle::NavStackCommandHandle()
 : _pimpl(rmf_utils::make_impl<Implementation>(Implementation()))
 {}
 
 //==============================================================================
-
 NavStackCommandHandle::~NavStackCommandHandle()
 {}
 
 //==============================================================================
-
-void NavStackCommandHandle::go_to_new_destination(
-  const rmf_traffic::agv::Plan::Waypoint& waypoint,
-  RequestCompleted destination_reached_callback)
+void NavStackCommandHandle::follow_new_path(
+    const std::vector<rmf_traffic::agv::Plan::Waypoint>& waypoints,
+    RequestCompleted path_finished_callback)
 {
+  ROS_INFO("Got a new path goal of length: %lu", waypoints.size());
+  
+  {
+    std::lock_guard<std::mutex> lock(_pimpl->_mutex);
+    _pimpl->_goal_path.clear();
+    for (std::size_t i = 0; i < waypoints.size(); ++i)
+    {
+      _pimpl->_goal_path.push_back(
+        Implementation::Goal {
+          waypoints[i],
+          _pimpl->_waypoint_to_move_base_goal(waypoints[i]),
+          false});
+    }
+  }
 
+  _pimpl->_move_base_client->cancelAllGoals();
+  std::lock_guard<std::mutex> lock(_pimpl->_mutex);
+  if (!_pimpl->_goal_path.empty())
+  {
+    const Implementation::Goal& next_goal = _pimpl->_goal_path.front();
+    if (!next_goal.sent)
+    {
+      const Eigen::Vector3d& pos = next_goal.waypoint.position();
+      ROS_INFO("sending next goal: %.3f %.3f %.3f", pos[0], pos[1], pos[2]);
+      _pimpl->_move_base_client->sendGoal(next_goal.goal);
+      _pimpl->_goal_path.front().sent = true;
+    }
+  }
 }
 
 //==============================================================================
-
 void NavStackCommandHandle::stop()
 {
-
+  _pimpl->_move_base_client->cancelAllGoals();
+  std::lock_guard<std::mutex> lock(_pimpl->_mutex);
+  if (!_pimpl->_goal_path.empty())
+    _pimpl->_goal_path[0].sent = false;
 }
 
 //==============================================================================
-
 void NavStackCommandHandle::resume()
 {
-
+  std::lock_guard<std::mutex> lock(_pimpl->_mutex);
+  if (!_pimpl->_goal_path.empty())
+  {
+    const Implementation::Goal& next_goal = _pimpl->_goal_path.front();
+    if (!next_goal.sent)
+    {
+      const Eigen::Vector3d& pos = next_goal.waypoint.position();
+      ROS_INFO("sending next goal: %.3f %.3f %.3f", pos[0], pos[1], pos[2]);
+      _pimpl->_move_base_client->sendGoal(next_goal.goal);
+      _pimpl->_goal_path.front().sent = true;
+    }
+  }
 }
 
 //==============================================================================
-
 void NavStackCommandHandle::dock(
   const std::string& dock_name,
   RequestCompleted docking_finished_callback)
